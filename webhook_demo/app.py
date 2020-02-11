fbba95624fdce5d20e05c83e101d8d7da9b868db
virusdefender <virusdefender@outlook.com>
2020-02-11 12:45:15 +0800

    update webhook_demo

--- webhook_demo/app.py
+++ webhook_demo/app.py
@@ -12,19 +12,25 @@ plugin_vuln_count = {}
 
 
 def process_vuln(data):
+    # 数据格式见 https://xray.cool/xray/#/api/vuln
     plugin = data["plugin"]
     if plugin not in plugin_vuln_count:
         plugin_vuln_count[plugin] = 1
     else:
         plugin_vuln_count[plugin] += 1
+
     # 在这里拿到漏洞数据之后还可以干很多事情，比如自行存入数据库、微信推送、钉钉推送等，相关文档可以参考
     # http://sc.ftqq.com/3.version
     # https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
-
+    print(f"{plugin} vuln +1")
 
 def process_statistics(data):
+    # 数据格式见 https://xray.cool/xray/#/api/statistic
     global statistics
     statistics = data
+
+    # 在这里拿到统计数据之后还可以干很多事情，比如 num_found_urls - num_scanned_urls == 0 可以认为扫描结束了
+    print(f"statistics {data}")
 
 
 # 访问首页就可以看到统计数据
@@ -38,10 +44,10 @@ def index():
 def xray_webhook():
     # FIXME 使用 token 来保证 api 安全
     data = request.json
-    # 目前是通过这个字段来区分统计数据和漏洞数据的，之后将在 http header 中加入字段区分
-    if "vuln_class" in data:
+    data_type = data.get("type")
+    if data_type == "web_vuln":
         process_vuln(data)
-    else:
+    elif data_type == "web_statistic":
         process_statistics(data)
     return "ok"
 
