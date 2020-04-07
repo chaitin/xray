42a1f0e0f022792258c8051510fff52e9f32444b
rawidN <raiwdn@163.com>
2020-04-07 13:46:33 +0800

    开源 HTML 报告

---
+++ report/webstorm.config.js
@@ -0,0 +1,16 @@
+'use strict'
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, '.', dir)
+}
+
+module.exports = {
+  context: path.resolve(__dirname, './'),
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': resolve('src')
+    }
+  }
+}
